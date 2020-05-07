5502046d66fe9ffe89e0edf6c71c4fe6d915940a
JordiRodriguezC <ycorrales120@gmail.com>
2020-05-06 19:58:59 -0600

    alertify

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -8,8 +8,10 @@ class Web extends CI_Controller
         parent::__construct();
         $this->load->model('Web_model');
         $this->load->library('session');
+        
     }
-
+    public $correo = false;
+    
     function index($sections_data = array())
     {
         if ($sections_data == null)
@@ -18,12 +20,16 @@ class Web extends CI_Controller
             $data['sections'] = $sections_data;
         $data['servicios'] = $this->Web_model->get_All_Services();
         $data['images'] = $this->Web_model->get_All_Images();
+        if($this->correo){
+            $data['message_display'] = 'Correo electrónico enviado exitosamente';
+        }
         $data['_view'] = 'web/index';
         $this->load->view('layouts/main', $data);
     }
 
     function correo()
     {
+        $this->correo = false;
         $this->load->library('form_validation');
         $this->form_validation->set_rules('email', 'Post/email', 'required|max_length[100]');
         $this->form_validation->set_rules('name', 'Post/name', 'required|max_length[50]');
@@ -35,8 +41,9 @@ class Web extends CI_Controller
                 'correo' => $this->input->post('email'),
                 'mensaje' => $this->input->post('message'),
             );
-
+           
             $this->Web_model->add_email($params);
+            $this->correo = true;
             $this->index();
         }
     }
